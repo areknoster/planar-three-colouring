3b1c80ef0d860af6c27d29c51674d8109216de1e
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2021-06-06 14:56:27 +0200

    simplify coloring checking

--- Planar3Coloring/Planar3Coloring.Test/ColoringChecker.cs
+++ Planar3Coloring/Planar3Coloring.Test/ColoringChecker.cs
@@ -6,17 +6,11 @@ namespace Planar3Coloring.Test
     {
         public static bool CheckColoring(UndirectedGraph<int, IEdge<int>> graph, GraphColor[] coloring)
         {
-            for (int i = 0; i < graph.VertexCount; i++)
+            foreach (var e in graph.Edges)
             {
-                var neighbors = graph.AdjacentVertices(i);
-                var c = coloring[i];
-                foreach (int n in neighbors)
+                if (coloring[e.Source] == coloring[e.Target])
                 {
-                    if (coloring[n] == c)
-                    {
-                        throw new Exception(
-                            $"coloring is incorrect: adjacent verices {i} and {n} are of the same color");
-                    }
+                    return false;
                 }
             }
             return true;
---
+++ Planar3Coloring/Planar3Coloring.Test/IRandomPlanarGraphGenerator.cs
@@ -0,0 +1,7 @@
+namespace Planar3Coloring.Test
+{
+    public interface RandomPlanarGraphgenerator
+    {
+        Ge
+    }
+}
