43a9ed42c15fb9d14b25ec29dab1e704f5251dc5
daveedoo <73546387+daveedoo@users.noreply.github.com>
2021-06-09 16:34:18 +0200

    Implement triangulation

--- Planar3Coloring/Planar3Coloring/ITriangulation.cs
+++
@@ -1,9 +0,0 @@
-﻿using QuikGraph;
-
-namespace Planar3Coloring
-{
-    public interface ITriangulation
-    {
-        public IGraph<int, IEdge<int>> Triangulate(IGraph<int, IEdge<int>> graph);
-    }
-}
---
+++ Planar3Coloring/Planar3Coloring/Triangulation/ITriangulation.cs
@@ -0,0 +1,9 @@
+﻿using QuikGraph;
+
+namespace Planar3Coloring.Triangulation
+{
+    public interface ITriangulation
+    {
+        public UndirectedGraph<int, IEdge<int>> Triangulate(UndirectedGraph<int, IEdge<int>> graph, int root = 0);
+    }
+}
