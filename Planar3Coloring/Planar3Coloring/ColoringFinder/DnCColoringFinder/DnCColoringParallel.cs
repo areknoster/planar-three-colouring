b9c4e55dee17b79504756462e43fcf0858ba2ad4
Michalski-Artur <michalskiartur621@gmail.com>
2021-06-12 15:19:58 +0200

    Add optimalization when component cannot be colored

--- Planar3Coloring/Planar3Coloring/ColoringFinder/DnCColoringFinder/DnCColoringParallel.cs
+++ Planar3Coloring/Planar3Coloring/ColoringFinder/DnCColoringFinder/DnCColoringParallel.cs
@@ -23,42 +23,40 @@ namespace Planar3Coloring.ColoringFinder.DnCColoringFinder
             _coloring = new GraphColor?[_graph.VertexCount];
 
             //Get all components of G
-            List<UndirectedGraph<int, IEdge<int>>> G_components = FindComponents(_graph);
-            foreach (UndirectedGraph<int, IEdge<int>> component in G_components)
+            (List<UndirectedGraph<int, IEdge<int>>> list, Dictionary<int, int> dict) G_components = FindComponents(_graph);
+            foreach (UndirectedGraph<int, IEdge<int>> component in G_components.list)
             {
                 //Find separator S for component
                 HashSet<int> S = PlanarSeparator.FindSeparator(component);
                 ////Split component into smaller components
                 foreach (int v in S)
                     component.RemoveVertex(v);
-                List<UndirectedGraph<int, IEdge<int>>> components = FindComponents(component);
+                (List<UndirectedGraph<int, IEdge<int>>>, Dictionary<int, int>) components = FindComponents(component);
 
                 //One of components cannot be colored => whole G cannot be colored
-                //Slow version
-                if (!DnCColoring(components, S))
+                if (!DnCColoring(components, S).isColorable)
                     return null;
-                //Fast version
-                //if (!DnCColoring(new List<UndirectedGraph<int, IEdge<int>>>(), _graph.Vertices.ToHashSet()))
-                //    return null;
             }
             return _coloring.Select(c => c.Value).ToArray();
         }
 
         public string Name => "DnCColoring";
 
-        private bool DnCColoring(List<UndirectedGraph<int, IEdge<int>>> components, HashSet<int> s)
+        private (bool isColorable, int? notColorableComponentIndex) DnCColoring((List<UndirectedGraph<int, IEdge<int>>> list, Dictionary<int, int> dict) components, HashSet<int> s)
         {
             //Separator colored
             if (s.Count == 0)
             {
                 bool canBeColored = true;
-                Parallel.ForEach(components, (UndirectedGraph<int, IEdge<int>> component, ParallelLoopState state) =>
+                int? notColorableComponentIndex = null;
+                Parallel.ForEach(components.list, (UndirectedGraph<int, IEdge<int>> component, ParallelLoopState state) =>
                 {
+                    notColorableComponentIndex = components.list.IndexOf(component);
                     if (component.VertexCount < 15)
                     {
                         //Can't find separator for component - color using bruteforce
-                        if (!DnCColoring(new List<UndirectedGraph<int, IEdge<int>>>(),
-                                                new HashSet<int>(component.Vertices)))
+                        if (!DnCColoring((new List<UndirectedGraph<int, IEdge<int>>>(), new Dictionary<int, int>()),
+                                                new HashSet<int>(component.Vertices)).isColorable)
                         {
                             canBeColored = false;
                             state.Break();
@@ -70,18 +68,19 @@ namespace Planar3Coloring.ColoringFinder.DnCColoringFinder
                         UndirectedGraph<int, IEdge<int>> componentCopy = component.Clone();
                         foreach (int vPrim in sPrim)
                             componentCopy.RemoveVertex(vPrim);
-                        List<UndirectedGraph<int, IEdge<int>>> componentsPrim = FindComponents(componentCopy);
-                        if (!DnCColoring(componentsPrim, sPrim))
+                        (List<UndirectedGraph<int, IEdge<int>>>, Dictionary<int, int>) componentsPrim = FindComponents(componentCopy);
+                        if (!DnCColoring(componentsPrim, sPrim).isColorable)
                         {
                             canBeColored = false;
                             state.Break();
                         }
                     }
                 });
-                return canBeColored;
+                return (canBeColored, notColorableComponentIndex);
             }
 
             //Separator still not colored
+            (bool isColorable, int? componentIndex) = (true, null);
             int v = s.First();
             s.Remove(v);
             foreach (GraphColor color in (GraphColor[])Enum.GetValues(typeof(GraphColor)))
@@ -116,20 +115,29 @@ namespace Planar3Coloring.ColoringFinder.DnCColoringFinder
                 if (moveToNextColor)
                     continue;
 
-                if (DnCColoring(components, s))
-                    return true;
+                if(!componentIndex.HasValue)
+                    (isColorable, componentIndex) = DnCColoring(components, s);
+                
+                if (isColorable)
+                    return (true, null);
 
                 //Reverse changes
                 foreach (int vertex in verticesWithTakenColor)
+                {
                     _availableColors[vertex].Add(color);
+                    if (componentIndex.HasValue && components.dict.ContainsKey(vertex) && components.dict[vertex] == componentIndex)
+                        componentIndex = null;
+                }
             }
             s.Add(v);
-            return false;
+            return (false, componentIndex);
         }
-        private List<UndirectedGraph<int, IEdge<int>>> FindComponents(UndirectedGraph<int, IEdge<int>> g)
+        private (List<UndirectedGraph<int, IEdge<int>>>, Dictionary<int, int>) FindComponents(UndirectedGraph<int, IEdge<int>> g)
         {
             List<UndirectedGraph<int, IEdge<int>>> components = new List<UndirectedGraph<int, IEdge<int>>>();
+            Dictionary<int, int> componentBelonging = new Dictionary<int, int>();
             HashSet<int> vertices = g.Vertices.ToHashSet();
+            int componentNumber = 0;
             while (vertices.Count > 0)
             {
                 UndirectedGraph<int, IEdge<int>> component = new UndirectedGraph<int, IEdge<int>>(false);
@@ -146,6 +154,7 @@ namespace Planar3Coloring.ColoringFinder.DnCColoringFinder
                         if (vertices.Contains(n))
                         {
                             component.AddVertex(n);
+                            componentBelonging.Add(n, componentNumber);
                             queue.Enqueue(n);
                             vertices.Remove(n);
                         }
@@ -153,8 +162,9 @@ namespace Planar3Coloring.ColoringFinder.DnCColoringFinder
                     }
                 }
                 components.Add(component);
+                componentNumber++;
             }
-            return components;
+            return (components, componentBelonging);
         }
     }
 }
